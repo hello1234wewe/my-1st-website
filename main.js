8dd90df61eb994e83811bbbf6e78ea0da7dc5d28
hello1234wewe <117018709+hello1234wewe@users.noreply.github.com>
2022-12-01 19:47:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function change1(){
+    document.getElementById("heading").style.color="#05cff7";
+}
+function changeimg(){
+    document.getElementById("image1").src="https://wallpaperaccess.com/full/11731.jpg";
+}
